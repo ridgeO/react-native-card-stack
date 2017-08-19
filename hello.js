8ec1fd40352d4740e0b79459decb5b4fbd38de5d
Ridge <rto@MBAir.local>
2017-08-18 18:01:50 -0700

    initialize repo

---
+++ hello.js
@@ -0,0 +1,11 @@
+'use strict';
+import React, { component } from 'react';
+import { Text } from 'react-native';
+
+export default class Hello extends Component {
+  render() {
+    return(
+      <Text>Hello!</Text>
+    )
+  }
+}
