77e1bfed840c88f4de71d12b94f5176974fb8b3e
Ridge <rto@MBAir.local>
2017-08-18 18:14:41 -0700

    fix capitalization error

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 'use strict';
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 export default class Hello extends Component {
